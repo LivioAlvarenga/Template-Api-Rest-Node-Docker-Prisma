d70150ee412ea46b9c839e8344b7cf7762eb3180
Livio Alvarenga <livioalvarenga@gmail.com>
2023-04-17 14:59:57 -0300

    add setErrorHandler of fastify

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { env } from './env'
 import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    //! TODO: Here we should log to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
